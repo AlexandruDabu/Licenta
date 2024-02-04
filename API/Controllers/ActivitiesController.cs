177d96b342aa26c330fc7487eb4b413c30a2d6f7
AlexandruDabu <alexandrufelix2020@gmail.com>
2024-02-04 12:39:03 +0200

    4th commit

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,4 +1,6 @@
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -8,21 +10,21 @@ namespace API.Controllers
 
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
+        private readonly IMediator _mediator;
+        public ActivitiesController(IMediator mediator)
         {
-            _context = context;
+            _mediator = mediator;
             
         }
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {   
-            return await _context.Activities.ToListAsync();
+            return await _mediator.Send(new List.Query());
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return Ok();
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Application.Activities;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -18,6 +19,8 @@ builder.Services.AddCors(opt =>{
         policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
     });
 });
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
