baebfc572a5eddff8fee2f8620f2037b12d3a7f9
AlexandruDabu <alexandrufelix2020@gmail.com>
2024-02-16 11:26:43 +0200

    16/2

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,32 @@
+import { action, makeAutoObservable, observable, runInAction } from "mobx";
+import { Activity } from "../models/activity";
+import agent from "../api/agent";
+
+export default class ActivityStore{
+activities: Activity[] = [];
+selectedActivity: Activity | null =null;
+editMode = false;
+loading = false;
+loadingInitial = false;
+constructor(){
+    makeAutoObservable(this)
+    
+}
+loadActivities = async () =>{
+    this.setLoadingInitial(true);
+    try {
+        const activities = await agent.Activities.list();
+            activities.forEach(activity =>{
+                activity.date = activity.date.split('T')[0];
+                this.activities.push(activity);
+              })
+              this.setLoadingInitial(false);
+    } catch (error) {
+        console.log(error);
+            this.setLoadingInitial(false);
+    }
+}
+setLoadingInitial = (state: boolean)=>{
+    this.loadingInitial = state;
+}
+}
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css'
 import App from './app/layout/App'
+import { StoreContext, store } from './app/stores/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <StoreContext.Provider value={store}>
     <App />
+    </StoreContext.Provider>
   </React.StrictMode>,
 )
