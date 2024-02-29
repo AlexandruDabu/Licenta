009ba5c1649a503df631e53ebe891b6b4abc0f23
AlexandruDabu <alexandrufelix2020@gmail.com>
2024-02-29 13:04:29 +0200

    Section 11 - Formik Handle

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -7,6 +7,7 @@ import 'react-toastify/dist/ReactToastify.min.css';
 import { StoreContext, store } from './app/stores/store'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app/Router/Routes'
+import 'react-datepicker/dist/react-datepicker.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <StoreContext.Provider value={store}>
