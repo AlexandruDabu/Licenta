5cc593ab3c71ff2b5980c93adc84a81abb63a8a4
AlexandruDabu <alexandrufelix2020@gmail.com>
2024-03-14 16:42:15 +0200

    Refresh bug fixed

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -21,7 +21,6 @@ namespace API.Services
                 new Claim(ClaimTypes.Name, user.UserName),
                 new Claim(ClaimTypes.NameIdentifier, user.Id),
                 new Claim(ClaimTypes.Email, user.Email),
-
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"]));
--- client-app/src/app/stores/commonStore.ts
+++ client-app/src/app/stores/commonStore.ts
@@ -3,7 +3,7 @@ import { ServerError } from "../models/serverError";
 
 export default class CommonStore{
     error: ServerError | null = null;
-    token: string | null = null;
+    token: string | null = localStorage.getItem('jwt');
     appLoaded = false;
 
     constructor(){
