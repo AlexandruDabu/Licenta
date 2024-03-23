c82b5572a9faefe409c8540afc64731fddfe8769
AlexandruDabu <alexandrufelix2020@gmail.com>
2024-03-23 21:48:51 +0200

    fly

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc8f6bd2ef808e62eb47fbc95c71b068f17a4100")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e6f334382a691d0f275460b4c13908cff4ca214")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
